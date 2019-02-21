9c8a381e9b7ae97238da35075453c0edb531ceec
ambiesoft <ambiesoft.trueff@gmail.com>
2019-02-21 18:03:03 +0900

    some

--- FileEditDialog/FormFileEdit.Designer.cs
+++ FileEditDialog/FormFileEdit.Designer.cs
@@ -37,10 +37,10 @@
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(424, 307);
+            this.btnOK.Location = new System.Drawing.Point(390, 296);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(95, 23);
-            this.btnOK.TabIndex = 0;
+            this.btnOK.TabIndex = 200;
             this.btnOK.Text = "&OK";
             this.btnOK.UseVisualStyleBackColor = true;
             // 
@@ -48,17 +48,16 @@
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(525, 307);
+            this.btnCancel.Location = new System.Drawing.Point(491, 296);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(95, 23);
-            this.btnCancel.TabIndex = 1;
+            this.btnCancel.TabIndex = 300;
             this.btnCancel.Text = "&Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // txtMain
             // 
             this.txtMain.AcceptsReturn = true;
-            this.txtMain.AcceptsTab = true;
             this.txtMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
@@ -66,8 +65,9 @@
             this.txtMain.MaxLength = 10485760;
             this.txtMain.Multiline = true;
             this.txtMain.Name = "txtMain";
-            this.txtMain.Size = new System.Drawing.Size(608, 286);
-            this.txtMain.TabIndex = 2;
+            this.txtMain.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtMain.Size = new System.Drawing.Size(574, 275);
+            this.txtMain.TabIndex = 100;
             // 
             // FormFileEdit
             // 
@@ -75,13 +75,15 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCancel;
-            this.ClientSize = new System.Drawing.Size(632, 339);
+            this.ClientSize = new System.Drawing.Size(598, 328);
             this.Controls.Add(this.txtMain);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(235, 198);
             this.Name = "FormFileEdit";
             this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "FormFileEdit";
             this.ResumeLayout(false);
